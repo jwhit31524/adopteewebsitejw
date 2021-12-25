b90051e05c414f6e7ac5eea0d0a68b0583bc2b73
jwhitton1 <jwhitton1@email.davenport.edu>
2021-12-25 10:45:02 -0500

    add files to initial repo

---
+++ Script/Script.js
@@ -0,0 +1,13 @@
+﻿
+/*search the html doc for the header id */
+var clickables = document.querySelector("#header");
+
+/*add an event listener for when the user clicks on the header*/
+clickables.addEventListener("click", showAlertBox, false)
+
+
+/*show alert when the user clicks on the header*/
+function showAlertBox() {
+    alert("Please click on the navigation buttons below instead of the header.");
+}
+
